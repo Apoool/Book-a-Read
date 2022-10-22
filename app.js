8b3643cf46e52f3c1f82ce7eb3a7e05758e8d0a0
Paul John Quiros <pauljohnquiros14@gmail.com>
2022-10-23 00:22:19 +0800

    Initial setup

---
+++ app.js
@@ -0,0 +1,30 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
+const db = mongoose.connection
+
+const homepageRouter = require('./routes/homepage')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+//MongoDB connection setup
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+})
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+app.use('/', homepageRouter)
+app.listen(PORT)
+console.log('Starting on port: ' + PORT)
